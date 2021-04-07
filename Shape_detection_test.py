50a37eaf574085dd0891fa4f022e9b410ee8e9a9
Nurium-Serium <73124417+Nurium-Serium@users.noreply.github.com>
2021-04-07 21:42:00 +0100

    Create Shape_detection_test.py

---
+++ Shape_detection_test.py
@@ -0,0 +1,45 @@
+import cv2 as cv
+img=cv.imread('groundtruth-rot180-7.png')
+gray=cv.cvtColor(img,cv.COLOR_BGR2GRAY)
+_,threshold = cv.threshold(gray, 20, 255, cv.THRESH_BINARY)
+contours,_ = cv.findContours(threshold, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
+threshold = cv.resize(threshold, (700, 700))
+cv.imshow('Hmmm',threshold)
+cv.waitKey(0)
+print(contours)
+for contour in contours:
+    approx = cv.approxPolyDP(contour, 0.01 * cv.arcLength(contour, True), True)
+    # using drawContours() function
+    cv.drawContours(img, [contour], 0, (0, 0, 255), 5)
+
+# finding center point of shape
+    M = cv.moments(contour)
+    if M['m00'] != 0.0:
+        x = int(M['m10'] / M['m00'])
+        y = int(M['m01'] / M['m00'])
+
+# putting shape name at center of each shape
+    if len(approx) == 3:
+        cv.putText(img, 'Triangle', (x, y),
+        cv.FONT_HERSHEY_SIMPLEX, 0.6, (100, 100, 255), 2)
+
+    elif len(approx) == 4:
+        cv.putText(img, 'Quadrilateral', (x, y),
+        cv.FONT_HERSHEY_SIMPLEX, 0.6, (100, 100, 255), 2)
+
+    elif len(approx) == 5:
+        cv.putText(img, 'Pentagon', (x, y),
+        cv.FONT_HERSHEY_SIMPLEX, 0.6, (100, 100, 255), 2)
+
+    elif len(approx) == 6:
+        cv.putText(img, 'Hexagon', (x, y),
+        cv.FONT_HERSHEY_SIMPLEX, 0.6, (100, 100, 255), 2)
+
+    else:
+        cv.putText(img, 'circle', (x, y),
+        cv.FONT_HERSHEY_SIMPLEX, 0.6, (100, 100, 255), 2)
+img = cv.resize(img, (700, 700))
+# displaying the image after drawing contours
+cv.imshow('shapes', img)
+cv.waitKey(0)
+cv.destroyAllWindows()
