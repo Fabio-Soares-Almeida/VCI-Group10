b3a111c00207da5fbfb97289a99c17f117838c34
Nurium-Serium <73124417+Nurium-Serium@users.noreply.github.com>
2021-04-12 11:42:04 +0100

    Update Shape_detection_test.py

--- Shape_detection_test.py
+++ Shape_detection_test.py
@@ -1,7 +1,8 @@
 import cv2 as cv
-img=cv.imread('groundtruth-rot180-7.png')
+img=cv.imread('groundtruth-rot0-4.png')
 gray=cv.cvtColor(img,cv.COLOR_BGR2GRAY)
-_,threshold = cv.threshold(gray, 20, 255, cv.THRESH_BINARY)
+#_,threshold = cv.threshold(gray, 120, 255, cv.THRESH_BINARY)
+threshold = cv.adaptiveThreshold(gray,255,cv.ADAPTIVE_THRESH_GAUSSIAN_C,cv.THRESH_BINARY,11,2)
 contours,_ = cv.findContours(threshold, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
 threshold = cv.resize(threshold, (700, 700))
 cv.imshow('Hmmm',threshold)
@@ -9,8 +10,8 @@ cv.waitKey(0)
 print(contours)
 for contour in contours:
     approx = cv.approxPolyDP(contour, 0.01 * cv.arcLength(contour, True), True)
-    # using drawContours() function
     cv.drawContours(img, [contour], 0, (0, 0, 255), 5)
+    # using drawContours() function
 
 # finding center point of shape
     M = cv.moments(contour)
@@ -37,7 +38,7 @@ for contour in contours:
 
     else:
         cv.putText(img, 'circle', (x, y),
-        cv.FONT_HERSHEY_SIMPLEX, 0.6, (100, 100, 255), 2)
+        cv.FONT_HERSHEY_SIMPLEX, 0.6, (100, 100, 255), 2) #Neste caso considera-se "circulo" a figuras geometricas com mais de 6 vertices
 img = cv.resize(img, (700, 700))
 # displaying the image after drawing contours
 cv.imshow('shapes', img)
